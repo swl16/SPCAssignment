7c19cbf932d5a2280297a3230cb7ea8f6c56fe73
swl16 <sweili165@gmail.com>
2026-08-02 00:42:23 +0800

    1

---
+++ User.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+struct Member {
+	string id;
+	string name;
+	int age;
+	string phNo;
+	char gender;
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+#include "Admin.cpp"
+#include "User.cpp"
+
+using namespace std;
+
+int main() {
+
+	cout << "================================" << endl;
+	cout << "\tFITNESS GYM" << endl;
+	cout << "================================" << endl;
+
+	return 0;
+}
