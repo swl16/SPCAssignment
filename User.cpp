bc7de005fdc0fca7f5a4beefbe1af5f37bb5f4db
Potatwoo-o <potatwoo0214@gmail.com>
2026-08-03 16:35:02 +0800

    yes

--- User.cpp
+++ User.cpp
@@ -2,6 +2,9 @@
 #include<iomanip>
 #include<vector>
 #include<string>
+#include<fstream>
+#include <sstream>
+
 using namespace std;
 
 
@@ -13,4 +16,81 @@ struct Member {
 	char gender;
 };
 
-static void userMenu() {}
+
+static void userMenu() {
+	char loginChoice;
+	string username;
+	string password;
+
+	cout << "=================================" << endl;
+	cout << "            USER MENU            " << endl;
+	cout << "=================================" << endl;
+
+	cout << "1. Register\n";
+	cout << "2. Login\n";
+	cout << "0. Exit\n";
+
+	do {
+		cout << "-------------------------";
+		cout << "Enter choice : ";
+		cin >> loginChoice;
+
+		switch (loginChoice) {
+		case '1':
+			cout << "================================\n";
+			cout << "            Register            \n";
+			cout << "================================\n";
+
+			cout << "Username :";
+			cin >> username;
+			cout << "Password :";
+			cin >> password;
+			
+			cout << "-----------------------------------------";
+			if (userExist) {
+				cout << "Register successful!";
+				cout << "Press ENTER to continue...";
+				userMenu();
+				break;
+			}
+			else {
+				cout << "Username exist! Please use another username.";
+			}
+				
+				
+
+				
+				
+				
+		}
+	} while (loginChoice != '0');
+		
+		
+}
+
+bool userExist(string targetUsername, string password) {
+	ifstream file("user.txt");
+
+	if (!file.is_open()) {
+		cerr << "Error: Could not open file." << endl;
+		return false;
+	}
+
+	string line, username, password;
+	while (getline(file, line)) {
+		stringstream ss (line);
+		if (ss >> username >> password) {
+			if (username == targetUsername) {
+				file.close();
+				return false; // Match found!
+			}
+		}
+	}
+
+	file.close();
+	return true;
+}
+
+void storeUser() {
+
+}
